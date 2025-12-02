531a0e36538c9e6b99702cce92ca8b4830524407
Herbertfrey <Herbertfrey@t-online.de>
2025-12-02 14:28:07 +0100

    Fix Supabase URL typo

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPA_URL = "https://mtybmrkyhavxpwysglo.supabase.co";
+const SUPA_URL = "https://mtybmrkyhavxpvwysglo.supabase.co";
 const SUPA_KEY = "dein public key";
 
 export const supabase = createClient(SUPA_URL, SUPA_KEY);
