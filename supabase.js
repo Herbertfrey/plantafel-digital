ccb9b51bb481dbddcbec42d8367959781468c7d4
Herbertfrey <Herbertfrey@t-online.de>
2025-12-02 13:22:18 +0100

    Update Supabase key to a placeholder value

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const SUPA_URL = "https://mtybmrkyhavxpwvysglo.supabase.co";
-const SUPA_KEY = "sb_publishable_0w8pZSpEAxxlv9Ke6dcurg_OuAl4q7r";
+const SUPA_KEY = "dein-Publicable-Key";
 
 export const supabase = createClient(SUPA_URL, SUPA_KEY);
