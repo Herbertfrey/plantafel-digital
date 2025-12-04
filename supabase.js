92218e67c7a0e50a3145ebac57be9233ccc6b76e
Herbertfrey <Herbertfrey@t-online.de>
2025-12-04 13:25:55 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPA_URL = "https://mtybmrkyhavxpvwysglo.supabase.co";
-const SUPA_KEY = "sb_publishable_0w8pZSpEAxxlv9Ke6dcurg_OuAl4q7r";
+const SUPA_URL = "https://yzfmviddzhghvcxowbjl.supabase.co";
+const SUPA_KEY = "sb_publishable_dacXIaV9sWHEN0ysSeoFuw_88UaV5tn";
 
 export const supabase = createClient(SUPA_URL, SUPA_KEY);
