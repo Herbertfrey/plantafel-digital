ca2bd907b6243ebe92bc2f639004714590425d52
Herbertfrey <Herbertfrey@t-online.de>
2025-12-12 09:19:52 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,9 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+// supabase.js (UMD / global) – KEIN import, KEIN module!
 
-const SUPABASE_URL = "https://yzfmviddzhghvcxowbjl.supabase.co";
-const SUPABASE_ANON_KEY =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6Zm12aWRkemhnaHZjeG93YmpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MjkyNzIsImV4cCI6MjA4MDQwNTI3Mn0.BOmbE7xq1-kUBdbH3kpN4lIjDyWIwLaCpS6ZT3mbb9U";
+// 1) HIER EINTRAGEN:
+const SUPABASE_URL = "HIER_DEINE_SUPABASE_URL_EINTRAGEN";
+const SUPABASE_ANON_KEY = "HIER_DEIN_ANON_KEY_EINTRAGEN";
 
-window.db = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// 2) Client erstellen
+// supabase ist global, weil index.html vorher das CDN lädt
+window.db = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
