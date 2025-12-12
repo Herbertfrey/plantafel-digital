38254269b64357ecc6cfe4b38a918963880037b3
Herbertfrey <Herbertfrey@t-online.de>
2025-12-12 09:09:50 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,7 @@
-// Supabase Client (Browser)
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-// Globale Supabase-Instanz
-window.db = createClient(
-  "https://yzfmviddzhghvcxowbjl.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6Zm12aWRkemhnaHZjeG93YmpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MjkyNzIsImV4cCI6MjA4MDQwNTI3Mn0.BOmbE7xq1-kUBdbH3kpN4lIjDyWIwLaCpS6ZT3mbb9U"
-);
+const SUPABASE_URL = "https://yzfmviddzhghvcxowbjl.supabase.co";
+const SUPABASE_ANON_KEY =
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6Zm12aWRkemhnaHZjeG93YmpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MjkyNzIsImV4cCI6MjA4MDQwNTI3Mn0.BOmbE7xq1-kUBdbH3kpN4lIjDyWIwLaCpS6ZT3mbb9U";
+
+window.db = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
