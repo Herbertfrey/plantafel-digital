e90d9474e5f94d33e12b6b2df4423793f461690f
Herbertfrey <Herbertfrey@t-online.de>
2025-12-12 09:04:11 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,9 +1,8 @@
-const SUPABASE_URL = "https://yzfmviddzhghvcxowbjl.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6Zm12aWRkemhnaHZjeG93YmpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MjkyNzIsImV4cCI6MjA4MDQwNTI3Mn0.BOmbE7xq1-kUBdbH3kpN4lIjDyWIwLaCpS6ZT3mbb9U";
+// Supabase Client (Browser)
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
+// Globale Supabase-Instanz
+window.db = createClient(
+  "https://yzfmviddzhghvcxowbjl.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6Zm12aWRkemhnaHZjeG93YmpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MjkyNzIsImV4cCI6MjA4MDQwNTI3Mn0.BOmbE7xq1-kUBdbH3kpN4lIjDyWIwLaCpS6ZT3mbb9U"
 );
-
-window.db = supabase;
