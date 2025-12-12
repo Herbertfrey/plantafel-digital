c5b8108834f1d70fb6f6720561a8d047a7c2f880
Herbertfrey <Herbertfrey@t-online.de>
2025-12-12 11:25:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // 🔐 SUPABASE KONFIGURATION
 const SUPABASE_URL = "https://yzfmviddzghvcxowbjl.supabase.co";
-const SUPABASE_ANON_KEY = "DEIN_ANON_KEY_HIER";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6Zm12aWRkemhnaHZjeG93YmpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MjkyNzIsImV4cCI6MjA4MDQwNTI3Mn0.BOmbE7xq1-kUBdbH3kpN4lIjDyWIwLaCpS6ZT3mbb9U";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
